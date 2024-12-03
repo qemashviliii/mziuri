91f8d41a69bf34bd54593bebf7e55743d9bbceaa
Lenovo <luka.q.luka@gmail.com>
2024-12-03 20:05:38 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String str1 = "";
+        for(int i = str.length();i>=0;i++){
+            str1+=i;
+        }
+        if(str.equals(str1)){
+            System.out.println("palindromia");
+        }else{
+            System.out.println("araa palindromi");
+        }
     }
-}
+    }
